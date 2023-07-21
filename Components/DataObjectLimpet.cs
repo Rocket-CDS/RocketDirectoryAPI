d9d804a7ed0c32e6df312361aff1e926e4861b9f
leedavi <dcl@agence-sesame.fr>
2023-07-21 04:40:36 +0200

    SystemSingleton

--- Components/DataObjectLimpet.cs
+++ Components/DataObjectLimpet.cs
@@ -30,7 +30,7 @@ namespace RocketDirectoryAPI.Components
             _passSettings = new Dictionary<string, string>();
             _dataObjects = new Dictionary<string, object>();
             var portalContent = new PortalCatalogLimpet(portalid, cultureCode, systemKey);
-            var systemData = new SystemLimpet(systemKey);
+            var systemData = SystemSingleton.Instance(_systemKey);
 
             SetDataObject("appthemesystem", AppThemeUtils.AppThemeSystem(portalid, systemKey));
             SetDataObject("appthemedirectory", AppThemeUtils.AppThemeSystem(portalid, "rocketdirectoryapi"));
