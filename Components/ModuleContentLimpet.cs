082685e226fad376b41f2b3571386012587f54fe
David Lee <dcl@agence-sesame.fr>
2024-10-23 12:48:52 +0200

    get module settings defaults

--- Components/ModuleContentLimpet.cs
+++ Components/ModuleContentLimpet.cs
@@ -8,29 +8,27 @@ namespace RocketDirectoryAPI.Components
 {
     public class ModuleContentLimpet : ModuleBase
     {
+        private PortalCatalogLimpet _portalContent; 
         public ModuleContentLimpet(int portalId, string moduleRef, string systemKey, int moduleid = -1, int tabid = -1) : base(portalId, moduleRef, moduleid, tabid)
         {
             base.SystemKey = systemKey;
+            _portalContent = new PortalCatalogLimpet(PortalId, DNNrocketUtils.GetCurrentCulture(), SystemKey);
+            base.AppThemeAdminFolder = _portalContent.AppThemeFolder;
+            base.AppThemeAdminVersion = _portalContent.AppThemeVersion;
+            base.ProjectName = _portalContent.ProjectName;
+
         }
         public int DefaultCategoryId { get { return GetSettingInt("defaultcategory"); } }
         public int ListPageTabId()
         {
             var rtn = GetSettingInt("listpage");
-            if (rtn == 0)
-            {
-                var p = new PortalCatalogLimpet(PortalId, DNNrocketUtils.GetCurrentCulture(), SystemKey);
-                rtn = p.ListPageTabId;
-            }
+            if (rtn == 0) rtn = _portalContent.ListPageTabId;
             return rtn;
         }
         public int DetailPageTabId()
         {
             var rtn = GetSettingInt("detailpage");
-            if (rtn == 0)
-            {
-                var p = new PortalCatalogLimpet(PortalId, DNNrocketUtils.GetCurrentCulture(), SystemKey);
-                rtn = p.DetailPageTabId;
-            }
+            if (rtn == 0) rtn = _portalContent.DetailPageTabId;
             return rtn;
         }
         public Dictionary<string, string> GetPropertyModuleGroups(CatalogSettingsLimpet catalogSettings)
